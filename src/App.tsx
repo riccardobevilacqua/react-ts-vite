bde48f419b288700950d8fbabd01466d096240e4
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2021-08-23 14:20:57 +0200

    Added emotion styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,88 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/react'
+import styled from '@emotion/styled'
 import React, { useState } from 'react'
 import logo from './logo.svg'
-
-const headerStyles = css({
-  backgroundColor: '#282c34',
-  minHeight: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  fontSize: 'calc(10px + 2vmin)',
-  color: 'white',
-})
-
-const logoStyles = css`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-      animation: App-logo-spin infinite 20s linear;
-  }
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`
-
-const linkStyles = css({
-  color: '#61dafb',
-})
 
 function App() {
   const [count, setCount] = useState(0)
 
   const handleClick = () => setCount((count) => count + 1)
 
+  const Container = styled.div`
+    text-align: 'center';
+  `
+
+  const Header = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    `
+
+  const Logo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+
+    @media (prefers-reduced-motion: no-preference) {
+      animation: App-logo-spin infinite 20s linear;
+    }
+
+    @keyframes App-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  `
+
+  const Button = styled.button`
+    font-size: calc(10px + 2vmin);
+  `
+
+  const Link = styled.a`
+    color: #61dafb;
+  `
+
   return (
-    <div css={{ textAlign: 'center' }}>
-      <header css={headerStyles}>
-        <img src={logo} css={logoStyles} alt="logo" />
+    <Container>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>Hello Vite + React + TypeScript!</p>
         <p>
-          <button
+          <Button
             type="button"
             onClick={handleClick}
-            css={{ fontSize: 'calc(10px + 2vmin)' }}>
+            >
             count is: {count}
-          </button>
+          </Button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
+          <Link
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-            css={linkStyles}
           >
             Learn React
-          </a>
+          </Link>
           {' | '}
-          <a
+          <Link
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
-            css={linkStyles}
           >
             Vite Docs
-          </a>
+          </Link>
         </p>
-      </header>
-    </div>
+      </Header>
+    </Container>
   )
 }
 
